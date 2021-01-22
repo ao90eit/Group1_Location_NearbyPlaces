93a56fa07cc12901c9bdf087033255bbd609a8d8
Adam Ormsby <adam.ormsby@enhanceit.us>
2021-01-22 01:12:03 -0500

    Initial commit, add some APIs

---
+++ app/src/main/java/com/aoinc/group1_location_nearbyplaces/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aoinc.group1_location_nearbyplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
