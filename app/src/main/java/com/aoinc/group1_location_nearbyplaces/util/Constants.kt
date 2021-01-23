c2b0ebc961e9826429819dcc569d7ea47dbe7d17
geolurez-eit <76801116+geolurez-eit@users.noreply.github.com>
2021-01-22 23:41:04 -0500

    Adding Retrofit implementation

--- app/src/main/java/com/aoinc/group1_location_nearbyplaces/util/Constants.kt
+++ app/src/main/java/com/aoinc/group1_location_nearbyplaces/util/Constants.kt
@@ -3,11 +3,15 @@ package com.aoinc.group1_location_nearbyplaces.util
 class Constants {
 
 
-    //-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=
-    companion object{
+    //-33.8670522,151.1957362&radius=1500&key=
+    //TODO figure out how to pass key, coordinate and radius parameters
+    companion object {
         const val BASE_URL = "https://maps.googleapis.com/"
         const val API_PATH = "/maps/api/place/nearbysearch/"
-        const val QUERY_NEARBY_LOCATIONS = "/json?location="
+        const val QUERY = "/json?"
+        const val QUERY_COORDINATES = "location="
+        const val QUERY_RADIUS = "&radius="
+        const val QUERY_KEY = "&key="
     }
 
 }
