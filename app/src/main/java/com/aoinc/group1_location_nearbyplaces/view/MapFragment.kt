7fb05afb38ce2e4eb9a7d98821a1e1e1d903a1f6
Adam Ormsby <adam.ormsby@enhanceit.us>
2021-01-24 19:19:43 -0500

    Add map fragment to map fragment
    Map loads on permission granted and moves to current location when updated

--- app/src/main/java/com/aoinc/group1_location_nearbyplaces/view/MapFragment.kt
+++ app/src/main/java/com/aoinc/group1_location_nearbyplaces/view/MapFragment.kt
@@ -10,12 +10,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
 import com.aoinc.group1_location_nearbyplaces.R
 import com.aoinc.group1_location_nearbyplaces.viewmodel.MapVMFactory
 import com.aoinc.group1_location_nearbyplaces.viewmodel.MapViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment(), LocationListener {
+class MapFragment : Fragment(), OnMapReadyCallback, LocationListener {
 
     // get same instance of view model
     private val viewModel: MapViewModel by viewModels(
@@ -23,6 +30,7 @@ class MapFragment : Fragment(), LocationListener {
     )
 
     private lateinit var locationManager: LocationManager
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +43,10 @@ class MapFragment : Fragment(), LocationListener {
     // this should only load if permission was given anyway
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.support_map_fragment) as SupportMapFragment
+//        val mapFragment = activity?.supportFragmentManager?.findFragmentById(R.id.support_map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         locationManager = view.context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000L, 20f, this)
@@ -50,5 +62,21 @@ class MapFragment : Fragment(), LocationListener {
     override fun onLocationChanged(location: Location) {
 //        Log.d("LOCATION_CHECK", "My location is: ${location.latitude}, ${location.longitude}")
         viewModel.updateNearbyLocations(location)
+
+        // check if lateinit var is initialized before setting location
+        if (this::mMap.isInitialized) {
+            val curLocation = LatLng(location.latitude, location.longitude)
+            mMap.addMarker(MarkerOptions().position(curLocation).title("My Location"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLocation, 15f))
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
